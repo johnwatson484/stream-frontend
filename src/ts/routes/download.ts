4d0b9eaa17a78718a11ff7e5606e553d2226c6f5
John Watson <john.watson1@defra.gov.uk>
2024-11-06 19:24:15 +0000

    Change postgres streaming

--- src/ts/routes/download.ts
+++ src/ts/routes/download.ts
@@ -1,6 +1,6 @@
 import { ServerRoute, Request, ResponseToolkit, ResponseObject } from '@hapi/hapi'
 import Wreck from '@hapi/wreck'
-import { Readable, Transform } from 'stream'
+import { Readable } from 'stream'
 import config from '../config.js'
 
 const routes: ServerRoute[] = [{
@@ -20,35 +20,25 @@ const routes: ServerRoute[] = [{
 }, {
   method: 'GET',
   path: '/download/postgres/stream',
-  handler: async (_request: Request, h: ResponseToolkit): Promise<any> => {
+  handler: async (_request: Request, h: ResponseToolkit): Promise<ResponseObject> => {
     const response: Readable = await Wreck.request('get', `${config.get('backendHost')}/postgres/stream`)
 
-    const transform: Transform = new Transform({
-      transform (chunk, _encoding, callback) {
-        try {
+    const stream: Readable = new Readable({
+      async read (_size) {
+        const chunk = await response.read()
+        if (chunk) {
           const jsonChunk = JSON.parse(chunk.toString())
           const rows = jsonChunk.data.map((row: any) => {
             return Object.values(row).join(',')
           }).join('\n')
           this.push(rows)
-          callback()
-        } catch (err: any) {
-          callback(err)
+        } else {
+          this.push(null)
         }
       }
     })
 
-    transform.on('error', (err) => {
-      console.error('Transform stream error:', err)
-    })
-
-    response.on('error', (err) => {
-      console.error('Response stream error:', err)
-    })
-
-    response.pipe(transform)
-
-    return h.response(transform)
+    return h.response(stream)
       .type('text/csv')
       .header('Content-Disposition', 'attachment; filename="data.csv"')
   }
